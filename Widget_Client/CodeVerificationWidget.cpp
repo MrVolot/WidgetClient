89ed32902a8da95ed3d2252118ff5fabc51ecfbb
Mr_Volot <kiril-kiril2003@ukr.net>
2023-06-05 22:42:37 +0300

    Style improvement

--- Widget_Client/CodeVerificationWidget.cpp
+++ Widget_Client/CodeVerificationWidget.cpp
@@ -8,6 +8,14 @@ CodeVerificationWidget::CodeVerificationWidget(QWidget *parent) :
     ui->setupUi(this);
     ui->verText->setWordWrap(true);
     ui->backButton->setDefault(false);
+
+    setStyleSheet(
+        "CodeVerificationWidget { background-color: #ffffff; padding: 16px; }"
+        "QLabel { color: #ffffff; font-size: 16px; margin-bottom: 8px; }"
+        "QLineEdit { height: 30px; font-size: 14px; padding: 4px; }"
+        "QPushButton { background-color: #4a90e2; color: #ffffff; font-size: 14px; border: none; border-radius: 4px; padding: 8px 16px; }"
+        "QPushButton:hover { background-color: #3887c9; }"
+        );
 }
 
 CodeVerificationWidget::~CodeVerificationWidget()
